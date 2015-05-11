a0b6f955e35492a851a327508033f4431ffc0f0c
Dzmitry Samsonau <demn_@tut.by>
2015-05-11 22:50:26 +0300

    Testing azure, travis and git for work

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ http.createServer(function (req, res) {
     res.writeHead(200, {
         'Content-Type': 'text/plain'
     });
-    res.end('Hello World\n');
+    res.end('Hello World\nAnd hello, Azure');
 }).listen(port);
 
 console.log('Server running at http://127.0.0.1:1337/');
