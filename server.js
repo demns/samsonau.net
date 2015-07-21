00d34c7f66808a98be4d67143d01316d6cb093a6
Dzmitry Samsonau <dzmitry_samsonau@epam.com>
2015-07-21 13:41:18 +0300

    Added bower, gulp, indentation according to editorconfig

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 var http = require('http');
 var port = process.env.PORT || 1337;
 
-http.createServer(function (req, res) {
-    res.writeHead(200, {
-        'Content-Type': 'text/plain'
-    });
-    res.end('Hello World\nAnd hello, Azure');
+http.createServer(function(req, res) {
+  res.writeHead(200, {
+    'Content-Type': 'text/plain'
+  });
+
+  res.end('Hello World\nAnd hello, Azure');
 }).listen(port);
 
 console.log('Server running at http://127.0.0.1:1337/');
