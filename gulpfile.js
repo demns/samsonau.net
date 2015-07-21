28b14591dddce2b5addc1a7ac20ba8660f0b014b
Dzmitry Samsonau <dzmitry_samsonau@epam.com>
2015-07-21 14:54:46 +0300

    Added runswquence to the gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@
 var gulp = require('gulp');
 var babel = require("gulp-babel");
 var nodemon = require('gulp-nodemon');
+var runSequence = require('run-sequence');
 var sass = require('gulp-sass');
 
 gulp.task('sass', function() {
@@ -15,19 +16,37 @@ gulp.task('sass:watch', function() {
   gulp.watch('./sass/**/*.scss', ['sass']);
 });
 
-gulp.task("default", function() {
+gulp.task("start", function() {
   return gulp.src("server.js")
     .pipe(babel())
     .pipe(gulp.dest("dist"));
 });
 
-gulp.task('start', function() {
-  nodemon({
-    script: 'dist/server.js',
-    ext: 'js html',
-    env: {
-      'NODE_ENV': 'development'
-    },
-    tasks: ['default, sass']
-  })
-})
+gulp.task('default', function gulpDefault() {
+  runSequence(
+    'start',
+    'sass',
+    'nodemon'
+  );
+});
+
+gulp.task('nodemon', function() {
+  return $.nodemon({
+      script: './server.js',
+      ext: 'js es6 hbs',
+      watch: [
+        'dist'
+        ],
+      env: process.env,
+      stdout: false
+    })
+    .on('readable', function() {
+      this.stdout.on('data', function(chunk) {
+        if (/listening/.test(chunk)) {
+          $.livereload.reload();
+        }
+        process.stdout.write(chunk);
+      });
+      this.stderr.pipe(process.stderr);
+    });
+});
