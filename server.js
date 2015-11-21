6e7476d930237a079133791d95299db83d43a35f
Dzmitry Samsonau <demn_@tut.by>
2015-11-22 00:50:31 +0300

    Change server.js to default one

--- server.js
+++ server.js
@@ -1,19 +1,6 @@
-//Lets require/import the HTTP module
-const http = require('http');
-
-//Lets define a port we want to listen to
-const port = 1337; 
-
-//We need a function which handles requests and send response
-function handleRequest(request, response) {
-  response.end('It Works!! Path Hit: ' + request.url);
-}
-
-//Create a server
-var server = http.createServer(handleRequest);
-
-//Lets start our server
-server.listen(port, function() {
-  //Callback triggered when server is successfully listening. Hurray!
-  console.log("Server listening on: http://localhost:%s", port);
-});
+var http = require('http')
+var port = process.env.PORT || 1337;
+http.createServer(function(req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello World\n');
+}).listen(port);
