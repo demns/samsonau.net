684cbbb408d68a5921979306db20f2cfef09685d
Dzmitry Samsonau <demn_@tut.by>
2015-11-22 12:08:17 +0300

    hmmm?

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
-//Lets require/import the HTTP module
-const http = require('http');
+// require('babel-core/register');
 
-//Lets define a port we want to listen to
-const port = process.env.PORT || 1337; 
+var express = require('express');
+var app = express();
 
-//We need a function which handles requests and send response
-function handleRequest(request, response) {
-  response.end('It Works!! Path Hit: ' + request.url);
-}
-
-//Create a server
-var server = http.createServer(handleRequest);
-
-//Lets start our server
-server.listen(port, function() {
-  //Callback triggered when server is successfully listening. Hurray!
-  console.log("Server listening on: http://localhost:%s", port);
+app.get('/', function (req, res) {
+  res.send('Hello World!');
 });
+
+app.set('port', process.env.PORT || 1337);
+
+var server = app.listen(app.get('port'), function onStart() {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Express server listening at http://%s:%s', host, port);
+});
+
