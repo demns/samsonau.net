169e247005934c8e4035d53d3d4f3732b4a8b1c0
Dzmitry Samsonau <demn_@tut.by>
2015-11-22 11:37:21 +0300

    Removed server.js because of adding express

---
+++ app.js
@@ -0,0 +1,17 @@
+require('babel-core/register');
+
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.set('port', process.env.NODE_PORT || 1337);
+
+var server = app.listen(app.get('port'), 'localhost', function onStart() {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Express server listening at http://%s:%s', host, port);
+});
