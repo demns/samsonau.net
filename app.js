80cd5ea996e9ae175252c6b7b392790954bc970e
Dzmitry Samsonau <dzmitrysamsonau@gmail.com>
2016-05-05 22:38:53 +0300

    Fixed port

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ app.get('*', function(req, res){
   res.sendfile(__dirname + '/public/index.html');
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(1337, function () {
+  console.log('Example app listening on port 1337!');
 });
 
 module.exports = app;
