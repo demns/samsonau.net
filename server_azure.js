68312d627c67b6ab597a34f9be76242b9f03fb73
Dzmitry Samsonau <dzmitrysamsonau@gmail.com>
2016-06-18 11:04:54 +0300

    Added logs for forever

--- server_azure.js
+++ server_azure.js
@@ -1,16 +1,19 @@
 const exec = require('child_process').exec;
 exec('forever stopall && npm run build', (error, stdout, stderr) => {
-  if (error) {
-    console.error(`exec error: ${error}`);
-    return;
-  }
-  console.log(`stdout: ${stdout}`);
-  console.log(`stderr: ${stderr}`);
-
-  exec('forever start server.js', (error, stdout, stderr) => {
     if (error) {
-      console.error(`exec error: ${error}`);
-      return;
+        console.error(`exec error: ${error}`);
+        return;
     }
-  });
+    console.log(`stdout: ${stdout}`);
+    console.log(`stderr: ${stderr}`);
+
+    exec('forever start server.js', (error, stdout, stderr) => {
+        if (error) {
+            console.error(`exec error: ${error}`);
+            return;
+        }
+
+        console.log(`stdout: ${stdout}`);
+        console.log(`stderr: ${stderr}`);
+    });
 });
