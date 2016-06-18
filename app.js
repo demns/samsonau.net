68efddb884075cce30df2f0ce6df2f3ad26a7d30
Dzmitry Samsonau <dzmitrysamsonau@gmail.com>
2016-06-18 10:18:49 +0300

    Added dev & prod config

--- app.js
+++ app.js
@@ -1,20 +1,7 @@
-var express = require('express');
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-var app = express();
-
-app.use(express.static('public'));
-
-app.get('/styles/style.css', function(req, res){
-  res.sendFile(__dirname + '/public/style.css');
-});
-
-app.get('*', function(req, res){
-  res.sendFile(__dirname + '/public/index.html');
-});
-
-app.listen(process.env.PORT || 1337, function () {
-  console.log('Example app listening on port 1337!');
-});
-
-module.exports = app;
-
+ReactDOM.render(
+  <div>(c) 2016</div>,
+  document.getElementById('react')
+);
