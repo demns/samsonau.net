9b7408464bd5dd6ce6ee0020a408f8941c9d5f4c
Dzmitry Samsonau <dzmitrysamsonau@gmail.com>
2017-03-18 22:14:46 +0300

    Added path to azure's npm

--- server_azure.js
+++ server_azure.js
@@ -1,28 +1,20 @@
-var path = require('path');
-var express = require('express');
-var webpack = require('webpack');
-var config = require('./webpack.config.dev');
-
-var app = express();
-var compiler = webpack(config);
-
-var port = process.env.PORT || 1337;
-
-app.use(require('webpack-dev-middleware')(compiler, {
-  noInfo: true,
-  publicPath: config.output.publicPath
-}));
-
-app.use(require('webpack-hot-middleware')(compiler));
-
-app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'index.html'));
-});
-
-app.listen(port, function onAppListening(err) {
-  if (err) {
-    console.error(err);
-  } else {
-    console.info('==> 🚧  Webpack development server listening on port %s', port);
+const exec = require('child_process').exec;
+const path = "D:\\Program Files (x86)\\npm\\4.1.2\\";
+exec(path + ' run clean' + path + ' npm run foreverStop', (error, stdout, stderr) => {
+  if (error) {
+    console.error(`exec error: ${error}`);
+    return;
   }
+  console.log(`stdout: ${stdout}`);
+  console.log(`stderr: ${stderr}`);
+
+  exec(path + 'npm run build &&' + path + ' npm run foreverStart', (error, stdout, stderr) => {
+    if (error) {
+      console.error(`exec error: ${error}`);
+      return;
+    }
+
+    console.log(`stdout: ${stdout}`);
+    console.log(`stderr: ${stderr}`);
+  });
 });
